45b54aee96bc93917d0be775750de9fe18132b51
AZARIA7 <moon_89_saga@icloud.com>
2025-07-07 21:59:52 +0300

    Add files via upload

---
+++ components/Header.tsx
@@ -0,0 +1,7 @@
+export default function Header() {
+  return (
+    <header className="w-full p-4 border-b shadow-sm text-center text-lg font-semibold bg-white">
+      Rentivox
+    </header>
+  );
+}
